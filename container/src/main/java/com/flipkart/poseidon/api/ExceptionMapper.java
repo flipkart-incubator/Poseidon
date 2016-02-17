1a5f6408f2d35e224101f6d6604fdde1d3dcbb24
shrey-garg <shrey.garg@flipkart.com>
2016-02-17 14:34:29 +0530

    throws IOException

--- container/src/main/java/com/flipkart/poseidon/api/ExceptionMapper.java
+++ container/src/main/java/com/flipkart/poseidon/api/ExceptionMapper.java
@@ -1,10 +1,11 @@
 package com.flipkart.poseidon.api;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by shrey.garg on 16/02/16.
  */
 public interface ExceptionMapper {
-    boolean map(Throwable e, HttpServletResponse response);
+    boolean map(Throwable e, HttpServletResponse response) throws IOException;
 }
