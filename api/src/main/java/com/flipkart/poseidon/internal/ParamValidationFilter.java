5b7ef9dbb6a30780615d9d9c18a06f2d53c00c10
shrey-garg <shrey.garg@flipkart.com>
2016-05-23 20:20:51 +0530

    Making params field optional in API

--- api/src/main/java/com/flipkart/poseidon/internal/ParamValidationFilter.java
+++ api/src/main/java/com/flipkart/poseidon/internal/ParamValidationFilter.java
@@ -55,8 +55,10 @@ public class ParamValidationFilter implements Filter {
     public void filterRequest(Request request, Response response) throws InternalErrorException, BadRequestException, ProcessingException {
         PoseidonRequest poseidonRequest = (PoseidonRequest) request;
         Map<String, Object> parsedParams = new HashMap<>();
-        parsedParams.putAll(validateParams(poseidonRequest, params.getRequired(), true));
-        parsedParams.putAll(validateParams(poseidonRequest, params.getOptional(), false));
+        if (params != null) {
+            parsedParams.putAll(validateParams(poseidonRequest, params.getRequired(), true));
+            parsedParams.putAll(validateParams(poseidonRequest, params.getOptional(), false));
+        }
         poseidonRequest.setAttribute(RequestConstants.PARAMS, parsedParams);
     }
 
