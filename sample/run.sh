7bd2210667fe84e2c0a7947e46550ba1678e06da
Pasupuleti Sree Rama Krishna Prasad <prasad.krishna@flipkart.com>
2021-01-22 00:40:47 +0530

    fixes

--- sample/run.sh
+++ sample/run.sh
@@ -2,4 +2,4 @@
 
 JAVA_ARGS="-Dcom.sun.management.jmxremote.port=3335 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
 
-java ${JAVA_ARGS} -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -cp "target/*:target/lib/*:src/main/resources" -Dlog4j.configurationFile=src/main/resources/external/log4j.xml -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector com.flipkart.poseidon.Poseidon src/main/resources/external/bootstrap.xml
+/Users/prasad.krishna/Downloads/jdk-16.jdk\ 2/Contents/Home/bin/java ${JAVA_ARGS} -cp "target/*:target/lib/*:src/main/resources" -Dlog4j.configurationFile=src/main/resources/external/log4j.xml -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector com.flipkart.poseidon.Poseidon src/main/resources/external/bootstrap.xml
